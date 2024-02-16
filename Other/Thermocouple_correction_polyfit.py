ff3a1e128fda9360712446566a75928d1959b0bf
Axel Johansson <axeljohansson@duck.com>
2024-02-16 08:18:56 +0100

    update temperature fitting with new values due to new amplifier gain

--- Other/Thermocouple_correction_polyfit.py
+++ Other/Thermocouple_correction_polyfit.py
@@ -9,39 +9,61 @@ Date: 2023-07-21
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Measured values for T115 handle with C115-007 cartridge
+x_t115 = [0, 500, 630, 734, 825, 930]
+y_t115 = [25, 240, 300, 360, 415, 446]
+
+#  Fit a polynomal equation with degree 2
+model = np.poly1d(np.polyfit(x_t115, y_t115, 2))
+plt.plot(x_t115,y_t115, '*')
+
+X_t115 = np.linspace(0, 1000, 20)
+Y_t115 = X_t115*X_t115*model[2] + X_t115*model[1] + model[0]
+
+plt.plot(X_t115,Y_t115, '*-')
+
+# Print eq
+temp = "Equation_T115: " + str(model[2]) + " * x^2 + " + str(model[1]) + "* x + " + str(model[0]) 
+print(temp)
+
 # Measured values for T210 handle with C210-007 cartridge
-x_t210 = [0, 525, 621, 713, 834, 988, 1155, 1355, 1533, 1680, 1742]
-y_t210 = [25, 168, 191, 206, 248, 279, 319, 364, 404, 434, 440]
+x_t210 = [0, 500, 570, 660, 740, 810, 880, 950, 1015, 1231]
+y_t210 = [25, 180, 198, 235, 263, 285, 307, 326, 345, 420]
 
-#  Fit a polynomal equation with degree 3
-model = np.poly1d(np.polyfit(x_t210, y_t210, 3))
-plt.plot(x_t210,y_t210, '*-')
+#  Fit a polynomal equation with degree 2
+model = np.poly1d(np.polyfit(x_t210, y_t210, 2))
+plt.plot(x_t210,y_t210, '*')
 
-X_t210 = np.linspace(0, 1800, 20)
-Y_t210 = X_t210*X_t210*X_t210*model[3] + X_t210*X_t210*model[2] + X_t210*model[1] + model[0]
+X_t210 = np.linspace(0, 1400, 20)
+Y_t210 = X_t210*X_t210*model[2] + X_t210*model[1] + model[0]
 
 plt.plot(X_t210,Y_t210, '*-')
 
 # Print eq
-temp = "Equation_T210: " + str(model[3]) + " * x^3 " + str(model[2]) + " * x^2 + " + str(model[1]) + "* x + " + str(model[0]) 
+temp = "Equation_T210: " + str(model[2]) + " * x^2 + " + str(model[1]) + "* x + " + str(model[0]) 
+print(temp)
+
+# Measured values for T245 handle with C245-945 cartridge
+x_t245 = [0,1220, 1570, 1980, 2450, 3270]
+y_t245 = [25, 170, 212, 256, 312, 410]
+
+#  Fit a polynomal equation with degree 2
+model = np.poly1d(np.polyfit(x_t245, y_t245, 2))
+plt.plot(x_t245,y_t245, '*')
+
+X_t245 = np.linspace(0, 3500, 20)
+Y_t245 = X_t245*X_t245*model[2] + X_t245*model[1] + model[0]
+
+plt.plot(X_t245,Y_t245, '*-')
+
+# Print eq
+temp = "Equation_T245: " + str(model[2]) + " * x^2 + " + str(model[1]) + "* x + " + str(model[0]) 
 print(temp)
 
 
-# Measured values for T245 handle with C245-945 cartridge
-x_t245 = [0,1690, 2150, 2360, 2780, 3200, 3700]
-y_t245 = [25, 200, 240, 265, 300, 348, 399, ]
 
-#  Fit a polynomal equation with degree 3
-model = np.poly1d(np.polyfit(x_t245, y_t245, 3))
-plt.plot(x_t245,y_t245, '*-')
 
-X_t245 = np.linspace(0, 3500, 20)
-Y_t245 = X_t245*X_t245*X_t245*model[3] + X_t245*X_t245*model[2] + X_t245*model[1] + model[0]
-
-plt.plot(X_t245,Y_t245, '*-')
 plt.grid(True)
 plt.show()
 
-# Print eq
-temp = "Equation_T245: " + str(model[3]) + " * x^3 " + str(model[2]) + " * x^2 + " + str(model[1]) + "* x + " + str(model[0]) 
-print(temp)
+
