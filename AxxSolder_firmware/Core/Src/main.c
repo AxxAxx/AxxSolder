86fcba14827ebc409fd51eb9046ceb0629655546
Axel Johansson <axeljohansson@duck.com>
2024-09-06 11:37:18 +0200

    Add heater_off() to Error_handler()

--- AxxSolder_firmware/Core/Src/main.c
+++ AxxSolder_firmware/Core/Src/main.c
@@ -2400,6 +2400,7 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
+	heater_off();
   __disable_irq();
   while (1)
   {
